3e9ff42bccc338cac18403302237b330b06f5518
tthugy <supermanace7@gmail.com>
2022-07-23 23:41:32 +0900

    update

--- pages/main.js
+++ pages/main.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import React, { useState } from 'react';
 import Link from 'next/link'
 import styles from '../styles/main.module.css'
+import Commit from '../public/myCommit.png'
+import CommitDk from '../public/myCommit_dark.png'
 
 export default function Header() {
 
@@ -13,7 +15,7 @@ export default function Header() {
     navigator.clipboard.writeText('cwstwin1@gmail.com');
     setTimeout(()=>{
       setLink(false)
-    }, 2000);
+    }, 3000);
   }
 
   return (
@@ -22,10 +24,10 @@ export default function Header() {
             <p>
               안녕하세요. 개발자 최원석입니다.
               <span className={styles.linkCollect}>
-                <a target='_blank' href='https://github.com/tthugy' rel='noreferrer'>
+                <a target='_blank' href='https://github.com/tthugy' rel='noopener noreferrer'>
                   <i className="bi bi-github"></i>
                 </a>
-                <a target='_blank' href='https://www.linkedin.com/in/wonseok-choi-b6041922b/' rel='noreferrer'>
+                <a target='_blank' href='https://www.linkedin.com/in/wonseok-choi-b6041922b/' rel='noopener noreferrer'>
                   <i className="bi bi-linkedin"></i>
                 </a>
                 <a onClick={linkClicked}>
@@ -50,10 +52,6 @@ export default function Header() {
           </ul>
         </div>
 
-        <div className={styles.project}>
-          <p>My Projects</p>
-        </div>
-
         <div className={styles.education}>
           <p>Educations</p>
           <span>백석대학교, 16.03 ~ 22.02</span>
@@ -61,6 +59,24 @@ export default function Header() {
             <li>주전공: 정보통신학</li>
             <li>복수전공: 컬처테크융합</li>
           </ul>
+        </div>
+
+        <div className={styles.commitBox}>
+          <p>Self Improvement</p>
+          <div className={styles.commitImg}>
+            <span className={styles.commit}>
+              <Image src={Commit} layout='responsive' objectFit='contain' priority />
+            </span>
+          </div>
+          <span className={styles.commitDesc}>
+            저는 대학교를 졸업한 이래로, 퇴근 후와 주말 등의 여가시간에 daily study와 commit을 목표로 꾸준한 자기계발을 해 왔습니다.
+            <br/><br/>
+            이를 증명할 수 있는 것이 <a target='_blank' href='https://github.com/tthugy' rel='noopener noreferrer'>제 Github 계정</a>이 아닐까 합니다. 위 이미지는 저의 Github 커밋 내역을 그래프화한 것입니다.
+          </span>
+        </div>
+
+        <div className={styles.project}>
+          <p>My Projects</p>
         </div>
     </main>
   )

578d991988759896dc38694aa8c54ee10036e5cf
tthugy <supermanace7@gmail.com>
2022-08-14 00:09:07 +0900

    update

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <div className="component">
       <Head>
-        <title>resume/Wonseock Choi</title>
+        <title>Resume/Wonseok Choi</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
         <link rel="shortcut" sizes="72x72" href="../public/favicion.ico" />
       </Head>

79a29125b377c57abe855b2624e17b00e355ccb3
tthugy <supermanace7@gmail.com>
2022-08-14 00:22:00 +0900

    update

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import Image from 'next/image'
 import { useRouter } from "next/router"
+import { favicon } from '../public/favicon.ico'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,7 +13,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Resume/Wonseok Choi</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
-        <link rel="shortcut" sizes="72x72" href="../public/favicion.ico" />
+        <link rel="shortcut" href={favicon} sizes="72x72" />
       </Head>
       <Component {...pageProps} />
     </div>

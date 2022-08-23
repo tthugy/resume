43a995051e1a819562f0d9d23ba38b0c3022d08c
tthugy <supermanace7@gmail.com>
2022-08-23 23:41:03 +0900

    update

---
+++ pages/components/modal.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+import Header from './header'
+import Main from './main'
+
+export default function Modai() {
+  return (
+    <div className="modal">
+        
+    </div>
+  )
+}
